d96c7cafd7d42ab572d9c2a5d1dfae4c4785fdc7
Boris Drazhzhov <templar8@gmail.com>
2016-02-02 17:24:17 +0300

    fixed deployment configuration: now all changes is being deployed to all staging servers

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 server '52.24.149.235', user: fetch(:user), roles: %w(app db)
+server '52.24.241.165', user: fetch(:user), roles: %w(app)
 set :rails_env, 'staging'
 set :branch, 'ror/staging'
 
