05141a57f770850b5f9c51cb7017bb6882a4202a
Boris Drazhzhov <templar8@gmail.com>
2016-02-10 15:31:03 +0300

    Fixed crash on GET /calendars/:id/items and updated docs

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,6 +1,6 @@
 class Calendar < ActiveRecord::Base
   belongs_to :user
-  has_many :calendar_items
+  has_and_belongs_to_many :calendar_items
   has_and_belongs_to_many :calendars_groups
 
   validates :title, length: {maximum: 128}, presence: true
