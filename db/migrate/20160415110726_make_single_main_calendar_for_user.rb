dfbcd7796ce33f94451a4893ec4e4deded6e0d13
Boris Drazhzhov <templar8@gmail.com>
2016-04-18 16:34:45 +0300

    FIxed issues with deployment failing

--- db/migrate/20160415110726_make_single_main_calendar_for_user.rb
+++ db/migrate/20160415110726_make_single_main_calendar_for_user.rb
@@ -1,5 +1,7 @@
 class MakeSingleMainCalendarForUser < ActiveRecord::Migration
   def change
-    add_index :calendars, [:main, :user_id], unique: true
+    add_column :profiles, :main_calendar_id, :integer, null: true
+    add_foreign_key :profiles, :calendars, column: :main_calendar_id, name: 'fk_main_calendar'
+    remove_column :calendars, :main, :integer, default: false, null: false
   end
 end
