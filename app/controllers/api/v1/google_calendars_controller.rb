38d56f57e209a0d4c39e8433cfc7339bfed30d95
koryushka <karetnikov.kirill@gmail.com>
2016-05-04 17:05:32 +0300

    Create event_recurrences during google calendar parsing

--- app/controllers/api/v1/google_calendars_controller.rb
+++ app/controllers/api/v1/google_calendars_controller.rb
@@ -33,18 +33,11 @@ class Api::V1::GoogleCalendarsController < ApiController
     @calendar_list = @service.list_calendar_lists
     calendars = []
     @calendar_list.items.each do |item|
-      calendar = Calendar.find_or_initialize_by(title: item.id, user_id: current_user.id) do |calendar|
-        # calendar.kind = item.kind
-
-      end
-      if calendar.new_record?
-        if calendar.save
-          calendars << calendar
-        end
+      calendar = Calendar.find_or_initialize_by(title: item.id, user_id: current_user.id)
+      if calendar.new_record? && calendar.save
+        calendars << calendar
       end
       parse_events(calendar)
-
-
     end
     render json: {events: @items}
     # render json: {imported: calendars}
@@ -53,35 +46,111 @@ class Api::V1::GoogleCalendarsController < ApiController
   private
 
   def parse_events(calendar)
+    @i ||= 0
     @items ||= []
     @service.list_events(calendar.title).items.each do |item|
-      if item.summary.blank?
-        puts "EVENT #{item.summary}"
-      end
-      event = Event.find_or_initialize_by(
-        starts_at: start_date(item) || Time.now,
-        title: item.summary || "new",
-        frequency: 'daily',
-        user_id: current_user.id
-        ) do |event|
-        end
-      if event.new_record?
-        if event.save
-          @items << event
-        end
-      end
+      @i += 1
+      puts "#{@i} - EVENT #{item.summary} - ID #{item.id}"
 
+        @event = Event.find_or_initialize_by(
+          starts_at: start_date(item),
+          title: title(item),
+          frequency: get_frequency(item),
+          user_id: current_user.id,
+          google_event_id: item.id
+        )
 
+      if @event.new_record? && @event.save
+        # puts "#{@i} FREQUENCE = #{@frequence}"
+
+      end
+      if @frequence && @event
+        calculate_event_recurrence
+      end
+      # create_event_cancellation
+      @frequence = nil if @frequence
+      @items << item
     end
+  end
 
+  def calculate_event_recurrence
+    case @frequence[:FREQ]
+      when 'DAILY'  then puts 'DAILY'
+      when 'WEEKLY' then create_weekly_event_recurrence
+      when 'MONTHLY'then create_monthly_event_recurrence
+      when 'YEARLY' then create_yearly_event_recurrence
+    end
+  end
+
+  def create_weekly_event_recurrence
+    days = @frequence[:BYDAY].split(',')
+    days.map do |day|
+      EventRecurrence.find_or_create_by(
+        event_id: @event.id,
+        month: nil,
+        week: nil,
+        day: get_day(day)
+      )
+    end
+  end
+
+  def create_monthly_event_recurrence
+    @frequence[:BYDAY].split(',').map do |day|
+      day.squish!
+      EventRecurrence.find_or_create_by(
+      event_id: @event.id,
+      week: day.slice!(0).to_i,
+      day: get_day(day)
+      )
+    end
+  end
+
+  def create_yearly_event_recurrence
+    date = @event.starts_at.to_date
+    EventRecurrence.find_or_create_by(
+      event_id: @event.id,
+      month: date.month,
+      week: nil,
+      day: date.day
+    )
+  end
+
+  def get_day(day)
+   week = {
+      'SU' => 0,'MO' => 1,'TU' => 2,'WE' => 3,'TH' => 4,'FR' => 5,'SA' => 6
+    }
+    week[day]
+  end
+
+  def get_frequency(item)
+    if item.recurrence
+      @frequence = count_frequency(item.recurrence[0])
+      @frequence[:FREQ].downcase
+    else
+      'once'
+    end
+  end
+
+  def count_frequency(recurrence)
+    rules = recurrence.gsub('RRULE:','').split(';')
+    hash = {}
+    rules.map do |r|
+      pair = r.split('=')
+      hash[pair[0].to_sym] = pair[1]
+    end
+    hash
   end
 
   def google_token_uri
-    'https://accounts.google.com/o/oauth2/token'
+    google_oauth2_path + '/token'
   end
 
   def google_oauth_uri
-    'https://accounts.google.com/o/oauth2/auth'
+    google_oauth2_path + '/auth'
+  end
+
+  def google_oauth2_path
+    'https://accounts.google.com/o/oauth2'
   end
 
   def oauth_params
@@ -105,8 +174,18 @@ class Api::V1::GoogleCalendarsController < ApiController
   end
 
   def start_date(item)
-    if item.start
+    if item.status == 'cancelled'
+      item.original_start_time.date_time
+    else
       item.start.date || item.start.date_time
+    end
+  end
+
+  def title(item)
+    if item.status == 'cancelled'
+      Event.find_by_google_event_id(item.recurring_event_id).title || 'No title'
+    else
+      item.summary
     end
   end
 
