0a1c458016af7fcc4ca853391da4ea34dd52618a
Daniil Leksin <danilaleksin@gmail.com>
2016-05-16 17:13:59 +0300

    Fix CUR-1444

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::DevicesController < ApiController
   include Swagger::Blocks
 
+  before_filter :find_entity, except: [:create]
+
   # swagger_path :Devices
-  swagger_path 'Devices' do
+  swagger_path '/devices' do
     operation :post do
       key :summary, 'Create device'
       key :description, 'Creates new device'
@@ -15,7 +17,12 @@ class Api::V1::DevicesController < ApiController
         end
       end
       # responses
-
+      response 201 do
+        key :description, 'Created'
+        schema do
+          key :'$ref', :Device
+        end
+      end # end response 201
       response 400 do
         key :description, 'Validation errors'
         schema do
@@ -57,61 +64,90 @@ class Api::V1::DevicesController < ApiController
     render nothing: true
   end
 
-  # # TODO: add to swagger_path
-  # def update
-  #
-  #   return render json: { errors: [{ message: t('missing_required_parameters'), code: 403 }]}, status: :bad_request unless params[:device_token]
-  #
-  #   if (params[:device_token] != @device.device_token)
-  #     @device.assign_attributes(device_params)
-  #     return render json: { validation_errors: @device.errors.messages }, status: :bad_request if @device.invalid?
-  #
-  #     begin
-  #       @sns.delete_endpoint(endpoint_arn: @device.aws_endpoint_arn)
-  #     rescue => e
-  #       return render json: { errors: [{ message: e.message }] }, status: :bad_request
-  #     end
-  #
-  #     begin
-  #       sns_response = insert_token(@device.aws_endpoint_arn)
-  #       if sns_response.present? && sns_response.http_response.status == 200
-  #
-  #         @device.aws_endpoint_arn = sns_response.endpoint_arn
-  #         @device.save
-  #       end
-  #     rescue => e
-  #       return render json: { errors: [{ message: e.to_s, code: 400 }]}, status: :bad_request
-  #     end
-  #
-  #     return render nothing: true
-  #   end
-  #
-  #
-  #   begin
-  #     @sns.set_endpoint_attributes(
-  #         endpoint_arn: @device.aws_endpoint_arn,
-  #         attributes: { 'Enabled' => 'true'}
-  #     )
-  #   rescue => e
-  #     return render json: { errors: [{message: e.message}] }, status: :bad_request
-  #   end
-  #
-  #   render nothing: true
-  # end
-  #
-  # # TODO: add to swagger_path
-  # def destroy
-  #   # removes device from database and from Amazon SNS registry
-  #   begin
-  #     @sns.delete_endpoint(endpoint_arn: @device.aws_endpoint_arn)
-  #   rescue => e
-  #     return render json: { errors: [{ message: e.message }] }, status: :bad_request
-  #   end
-  #
-  #   @device.destroy
-  #
-  #   render nothing: true
-  # end
+
+  # swagger_path device/{id}
+  swagger_path '/device/{id}' do
+    operation :put do
+      key :summary, 'Update device'
+      key :description, 'Updates device information by ID'
+      parameter do
+        key :name, 'id'
+        key :description, 'Device ID'
+        key :in, 'path'
+        key :required, true
+        key :type, :integer
+      end
+      parameter do
+        key :name, 'device'
+        key :in, 'body'
+        key :required, true
+        schema do
+          key :'$ref', :DeviceInput
+        end
+      end
+      # responses
+      response 201 do
+        key :description, 'Updated'
+        schema do
+          key :'$ref', :DeviceInput
+        end
+      end # end response OK
+      response 400 do
+        key :description, 'Validation errors'
+        schema do
+          key :'$ref', :ValidationErrorsContainer
+        end
+      end
+      # response Default
+      response :default do
+        key :description, 'Unexpected error'
+        schema do
+          key :'$ref', :Error
+        end
+      end # end response Default
+      key :tags, ['Devices']
+    end # end operation :put
+    operation :delete do
+      key :summary, 'Delete device'
+      key :description, 'Deletes device by ID'
+      parameter do
+        key :name, 'id'
+        key :description, 'Device ID'
+        key :in, 'path'
+        key :required, true
+        key :type, :integer
+      end
+      response 204 do
+        key :description, 'Deleted'
+      end
+      response :default do
+        key :description, 'Unexpected error'
+        schema do
+          key :'$ref', :Error
+        end
+      end # end response default
+      key :tags, ['Devices']
+    end # end operation :delete
+  end # end swagger_path /device/{id}
+
+  def update
+    @device.assign_attributes(device_params)
+    @sns.delete_endpoint(endpoint_arn: @device.aws_endpoint_arn)
+    sns_response = insert_token(@device.aws_endpoint_arn)
+    if sns_response.present? && sns_response.http_response.status == 200
+      # save new endpoint
+      @device.aws_endpoint_arn = sns_response.endpoint_arn
+      raise InternalServerErrorException unless @device.save
+    end
+    render nothing: true
+  end
+
+  # TODO: add to swagger_path
+  def destroy
+    @sns.delete_endpoint(endpoint_arn: @device.aws_endpoint_arn)
+    @device.destroy
+    render nothing: true, status: :no_content
+  end
 
 private
   def device_params
@@ -121,11 +157,9 @@ private
     :aws_endpoint_arn)
   end
 
-
   # Insert device token to SNS
   def insert_token(device_token)
-    ApiHelper::sns
-    sns.create_platform_endpoint(
+    @sns.create_platform_endpoint(
         platform_application_arn: "iOS",
         token: device_token,
     )
@@ -133,6 +167,7 @@ private
 
   # Remove end_point from SNS
   def delete_endpoint(end_point)
-    sns.delete_endpoint(endpoint_arn: end_point)
+    @sns.delete_endpoint(endpoint_arn: end_point)
   end
+
 end
