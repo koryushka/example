2cc2ab82f0fae985722b4782db053ecfe70d2b63
Daniil Leksin <danilaleksin@gmail.com>
2016-05-16 17:21:23 +0300

    Add test to update/delete calendar

--- test/controllers/Api/V1/devices_controller_test.rb
+++ test/controllers/Api/V1/devices_controller_test.rb
@@ -16,4 +16,26 @@ class DeviceControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'should fail invalid device creation' do
+    post :create
+    assert_response :bad_request
+  end
+
+  #### device update
+  test 'should update existing device' do
+    existing_user = FactoryGirl.create(:user)
+    device = FactoryGirl.create(:device, user: existing_user)
+    new_aws_endpoint_arn = 'aws:sns:us-west-2:123456789012:MyTopic:2bcfbf39-05c3-41de-xxxx-xxxxxxxxxxxx'
+    put :update, id: device.id, device_token: device.device_token, aws_endpoint_arn: new_aws_endpoint_arn
+    assert_response :success
+    assert_equal json_response['aws_endpoint_arn'], new_aws_endpoint_arn
+    assert_not_equal json_response['aws_endpoint_arn'], device.aws_endpoint_arn
+  end
+
+  test 'should fail device update with invalid data' do
+    existing_user = FactoryGirl.create(:user)
+    device = FactoryGirl.create(:device, user: existing_user)
+    put :update, id: device.id, device_token: device.device_token, aws_endpoint_arn: nil
+    assert_response :bad_request
+  end
 end
