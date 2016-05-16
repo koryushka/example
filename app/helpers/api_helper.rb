d9a862ffd25e9a8f0ae163c437766999d211382b
Daniil Leksin <danilaleksin@gmail.com>
2016-05-16 10:11:27 +0300

    Fix CUR-1442 after pull req. review

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -1,17 +1,6 @@
 module ApiHelper
-  # SNS connection
-  def self.sns
-    @sns = AWS::SNS::Client.new(
-        # TODO: Need to define region, access_key_id, secret_access_key
-        region: 'us-east-1',
-        access_key_id: 14,
-        secret_access_key: 'ABCDEF1234567890ABCDEF1234567890ABCDEF1234567890ABCDEF1234567890',
-    )
-  end
-
   # Send push notification
   def self.send(device_token, payload)
-    ApiHelper::sns
     notification_object = { object: payload }
 
     message = {
---
+++ config/initializers/aws_sns.rb
@@ -0,0 +1,7 @@
+Rails.application.configure do
+  @sns = AWS::SNS::Client.new(
+      region: ENV['AWS_REGION'] || 'us-west-2',
+      access_key_id: ENV['AWS_ACCESS_KEY'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  )
+end
