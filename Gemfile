14ad53fd142d10d3a3993fa34f635d2981a0e384
Daniil Leksin <danilaleksin@gmail.com>
2016-05-17 16:44:07 +0300

    change to actual aws-sdk gem

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'cancancan', '~> 1.10'
 gem 'pubnub'
 gem 'aws-ses', '~> 0.6.0', require: 'aws/ses'
 gem 'swagger-blocks'
-gem 'aws-sdk', '< 2.0'
+gem 'aws-sdk'
 
 
 # Use Capistrano for deployment
--- config/initializers/aws_sns.rb
+++ config/initializers/aws_sns.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  @sns = AWS::SNS::Client.new(
+  @sns = Aws::SNS::Client.new(
       region: ENV['AWS_REGION'] || 'us-west-2',
       access_key_id: ENV['AWS_ACCESS_KEY'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
