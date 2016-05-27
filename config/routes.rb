2f4b65b40a8b530aabb6917d93a670cfd23edb4b
koryushka <karetnikov.kirill@gmail.com>
2016-05-27 14:40:21 +0300

    added endpoint for web & iOS

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     #mount_devise_token_auth_for 'Admin', at: 'auth'
     root to: 'home#index'
   end
-
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       use_doorkeeper do
