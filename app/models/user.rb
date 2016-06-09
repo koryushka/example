526c4aab80727c3390eae547be80f7cca1eb80c8
koryushka <karetnikov.kirill@gmail.com>
2016-06-09 16:11:00 +0300

    added notification endpoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   has_many :participations
   has_many :devices, dependent: :destroy
   has_many :google_access_tokens, dependent: :destroy
+  has_many :google_channels, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
