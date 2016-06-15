95b640549ab18545bc1eaed9293aaccc981eb29a
koryushka <karetnikov.kirill@gmail.com>
2016-06-15 14:31:12 +0300

    added sidekiq initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://52.34.185.1:6379/12' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://52.34.185.1:6379/12' }
+end
